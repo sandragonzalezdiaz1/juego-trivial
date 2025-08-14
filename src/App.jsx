680e5d97f174799c4648f1a8b6f494985c055b71
Sandra González <sandragonzalezdiaz98@hotmail.com>
2025-08-14 11:23:01 +0200

    Juego trivial con preguntas aleatorias

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import preguntas from "./data/preguntas";
+import './App.css'
+import { useEffect, useState } from "react";
+import { Title, Row, TextQuestion, Box, Category } from "./styled";
+
+//Funcion para desordenar las preguntas
+const mezclarPreguntas = (array) => {
+  const copia = [...array]; //Spread operator: Hace una copia superficial del array, sin crear referencias al array original
+  for (let i = copia.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random()*(i + 1)); //Sumamos 1 para que incluya el maximo
+    [copia[i], copia[j]]= [copia[j], copia[i]]; //Algoritmo Fisher yates: para desordenar/barajear elementos del array
+
+  }
+
+  return copia; 
+
+}
+
+function App() {
+
+  const [preguntasBarajadas, setPreguntas] = useState([]);
+  const [preguntaActual, setPreguntaActual] = useState(0);
+  const [mensaje, setMensaje] = useState("");
+
+  //Mezcla las preguntas solo una vez al cargar la app
+  useEffect(() => {
+    setPreguntas(mezclarPreguntas(preguntas));
+  }, []);
+
+  if(preguntasBarajadas.length === 0) return <div>Cargando...</div>
+
+  //const pregunta = preguntas[preguntaActual];
+  const pregunta = preguntasBarajadas[preguntaActual];
+
+  const respuestaCorrecta = (opcion) => {
+    if(opcion === pregunta.respuesta){
+      setMensaje("Respuesta correcta👌");
+
+      // Cambiar de pregunta después de un pequeño delay
+      setTimeout(() => {
+         if(preguntaActual < preguntasBarajadas.length - 1){
+          setPreguntaActual((prev) => prev + 1);
+          setMensaje("");
+
+        } else { //Si nos encontramos en la ultima pregunta
+          setPreguntaActual(0); //Volvemos a la primera pregunta
+          setMensaje(""); //Limpiar mensaje
+        }
+      }, 1000);
+
+    } else {
+      setMensaje("La respuesta no es correcta, inténtalo de nuevo");
+    }
+  }
+
+  return (
+    <div>
+      <Title>TRIVIAL</Title>
+      <Category>Categoría: {pregunta.categoria}</Category>
+      <Row>
+        <TextQuestion>{pregunta.pregunta}</TextQuestion>
+        {
+          pregunta.img.endsWith(".mp4") ? (
+          <video controls style={{width:'100%', marginTop: '20px'}}>
+            <source src={pregunta.img} type="video/mp4" />
+          </video>
+          ) : 
+          pregunta.img.endsWith(".mp3") ? (
+          <audio controls style={{width:'100%', marginTop: '20px'}}> 
+            <source src={pregunta.img} type="audio/mpeg" />
+          </audio>
+          ) : (
+          <img src={pregunta.img} alt="Imagen de la pregunta" style={{width:'100%', marginTop: '20px'}}/>
+          )
+        }
+      <Box>
+        {pregunta.opciones.map((opcion, indice) => {
+          return <p key={indice}><button onClick={() => respuestaCorrecta(opcion)}>X</button>{opcion}</p>
+        })
+
+        }
+      </Box>
+      {mensaje && <p style={{fontWeight: "bold", fontSize:"24px"}}>{mensaje}</p>}
+      </Row>
+    </div>
+  )
+}
+
+export default App
---
+++ src/styled.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    text-align: center;
+    font-size: 3.5rem;
+    font-weight: bold;
+    color: white;
+    background: linear-gradient(90deg, #1d7ae4, #4bb0ff);
+    padding: 20px;
+    border-radius: 12px;
+    box-shadow: 0 4px 10px rgba(0,0,0,0.2);
+    margin: 20px auto;
+    max-width: 800px;
+    letter-spacing: 2px;
+`;
+
+export const Category = styled.p`
+     text-align: center;
+    font-size: 1.5rem;
+    color: #1d7ae4;
+    font-weight: 600;
+    margin-top: 10px;
+    margin-bottom: 25px;
+    font-style: italic;
+   
+`;
+
+export const Row = styled.div`
+    display: grid; 
+     grid-template-columns: 50% 50%; 
+    max-width: 800px;
+    margin: auto;
+    align-items: center;
+`;
+
+export const TextQuestion = styled.p`
+
+    font-size: 2rem; /*Duplica el tamaño de la fuente respecto al tamaño original */
+    font-weight: bold;
+    text-shadow: 1px 1px 1px gray; 
+    text-align: center;
+    margin-right: 40px;
+
+`;
+
+
+export const Box = styled.div`
+    font-size: 1.5rem;
+    padding-left: 35px;
+
+    p {
+        display: flex;
+        align-items: center;
+
+        button {
+            margin-right: 14px;
+            font-weight: bold;
+            font-size: 2rem;
+        }
+    }
+`;
